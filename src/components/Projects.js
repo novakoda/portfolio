07b0a20ccbfd3081a911d2e626e4ade75b8ec48d
JR Nova <novakoda9@gmail.com>
2021-01-07 21:42:25 -0600

    created project section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import projectData from './projectData.js';
 
 export default function Projects() {
+  const projects = [];
+
+  projectData.map(p => {
+    projects.push(
+      <div>
+        <h2>{p.name}</h2>
+        <img src={p.source}></img>
+        <a href={p.github}>GitHub</a>
+        <a href={p.demo}>Demo</a>
+      </div>
+    )
+  });
+  
   return (
     <div>
-
+      {projects}
     </div>
   )
 }
---
+++ src/components/projectData.js
@@ -0,0 +1,11 @@
+const projectData = [
+  {
+    id: 0,
+    name: 'Novabook',
+    source: `/projects/novabook.gif`,
+    github: 'https://github.com/novakoda/nb',
+    demo: 'https://novabook.herokuapp.com/'
+  },
+];
+
+export default projectData;
