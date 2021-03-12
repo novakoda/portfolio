8797e8858d9e88d92964e6d655b91102f4deb59b
JR Nova <novakoda9@gmail.com>
2021-03-12 16:03:04 -0600

    changed bio

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ export default function About() {
         <div className="align-self-center col-md-6 col-lg-8 pl-lg-3">
           <h1 style={{fontSize: 70}} className="text-center text-sm-left">James Williams</h1>
           <h3>About Me</h3>
-          <p>I'm a 25 year old full stack web developer, graphic designer and audio engineer from Houston, Texas. Currently taking classes for an Associates Degree in Computer Science at Houston Community College. The tech guy that you need to succeed lol help me write dis shit gahdamnit idk wtf to say on dis shit, but I do need something here preferably about this long!</p>
+          <p>I'm a 25 year old full stack web developer, graphic designer and audio engineer from Houston, Texas. Currently pursuing a bachelors in Computer Science. I love learning new technologies and with my background in graphic design and audio engineering, I'm well acquainted to bringing out creative solutions to complex problems. When I'm not behind a screen, I enjoy playing the guitar and piano, camping and kayaking. I hope you enjoy my projects!</p>
         </div>
       </div>
     </div>
