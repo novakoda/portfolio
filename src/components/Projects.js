d00e879374fbf0c10cd640e3c2faaf7bb41718db
JR Nova <novakoda9@gmail.com>
2021-07-19 17:27:47 -0500

    added info for novabook login

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,7 +14,19 @@ export default function Projects() {
               onMouseOut={e => ( e.currentTarget.src = p.pic )}
             ></img>
           <div className="card-body p-3">
-            <h5 className="mx-auto text-center">{p.name}</h5>
+            <h5 className="card-title text-center">{p.name}</h5>
+              { p.id === 0 &&
+                <div>
+                  <div class="text-info">
+                    <p class="card-title">Example Profile Login</p>
+                    <p class="card-text my-0">Email: john@doe.com</p>
+                    <p class="card-text my-0">Password: password</p>
+                  </div>
+                  <div class="text-danger">
+                    <p class="card-text">Note - Due to limitations with Heroku hosting, profile pictures are only saved for the duration of the session</p>
+                  </div>
+                </div>
+              }
             <div className="project-btns mt-2 row justify-content-between gap-2">
               <a href={p.demo} target="_blank" className="project-demo col-xl col-md-12 col-sm btn btn-primary align-middle btn-lg">Demo</a>
               <a href={p.github} target="_blank" className="project-code col-xl col-md-12 col-sm btn btn-outline-secondary btn-lg">Source Code</a>
